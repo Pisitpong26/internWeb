f50048af001517e329564a4937c943aa43e09143
Sherlock <pisitpong26@gmail.com>
2024-02-07 17:19:54 +0700

    edit

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,4 @@ const nextConfig = {
     output: 'export',
 };
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-    basePath: '/internWeb', // Adjust this to match your repository name
-};
+export default nextConfig;
